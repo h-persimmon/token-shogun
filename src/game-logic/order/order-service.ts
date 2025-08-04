4f57f5d01206a842147911bd3ca4cb691330de4d
SugitaMotoki <ee197011@meiji.ac.jp>
2025-08-04 23:11:33 +0900

    ordrerServiceの仮実装

--- src/game-logic/order/order-service.ts
+++ src/game-logic/order/order-service.ts
@@ -1,5 +1,7 @@
-import { AiResponseBody } from "@/api-interface/ai/responce-body";
+import { OrderResponseBody } from "@/api-interface/order/response-body";
 import { UnitService } from "../unit/unit-service";
+import { GameEngine } from "../engine";
+import { OrderRequestBody } from "@/api-interface/order/request-body";
 
 /**
  * 命令に関するサービス
@@ -13,19 +15,41 @@ export class OrderService {
      * ユニットに関するサービス
      */
     private readonly unitService: UnitService,
-  ) { }
+  ) {};
 
   /**
-   * AIにメッセージを送る関数
-   * @param prompt プロンプト
+   * AIに命令してゲーム状況を更新する関数
+   * @param userPrompt ユーザプロンプト
+   * @param gameEngine ゲームエンジン
+   */
+  public async order(userPrompt: string, gameEngine: GameEngine): Promise<void> {
+    const orderResponseBody = this.sendPromptToServer(userPrompt);
+    console.log((await orderResponseBody).output.message.content);
+    // TODO
+  }
+
+  /**
+   * ゲーム状況を表すテキストを作成する関数
+   * @param gameEngine ゲームエンジン
+   * @returns ゲーム状況を表すテキスト
+   */
+  public createGameStatisText(gameEngine: GameEngine): string {
+    return "";
+  }
+
+  /**
+   * サーバにユーザプロンプトとゲーム状況を送信する関数
+   * @param prompt ユーザプロンプト
+   * @param gameEngine ゲームエンジン
    * @returns レスポンスボディ
    */
-  public async sendMessageToAi(prompt: string): Promise<AiResponseBody> {
-    const url = "/api/ai"
-    const response = await fetch(url, {
+  private async sendPromptToServer(prompt: string): Promise<OrderResponseBody> {
+    const url = "/api/order";
+    const orderRequestBody: OrderRequestBody = { prompt };
+    const responce = await fetch(url, {
       method: "POST",
-      body: JSON.stringify({ prompt })
+      body: JSON.stringify(orderRequestBody),
     })
-    return response.json()
+    return responce.json();
   }
 }

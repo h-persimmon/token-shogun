f3e6240437b33087183bdbfe1f93551ecae52d13
id157 <id2000@outlook.jp>
2025-09-06 09:01:15 +0900

    add document

--- apps/platform/src/app/stages/page.tsx
+++ apps/platform/src/app/stages/page.tsx
@@ -4,6 +4,8 @@ export default function Page() {
   return (
     <div>
       <a href="game/1">ゲーム</a>
+      {/* 以下のコンポーネントは、monorepoの別パッケージから持ってきたもの */}
+      {/* propsとして、csvFilePathを渡すと、その設定でゲームが開始する = ステージ選択を反映できる */}
       <Game />
     </div>
   );

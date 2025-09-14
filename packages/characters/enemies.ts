0959caeb7cd74628a0e2c58e34d4479c8040d46b
id157 <id2000@outlook.jp>
2025-09-15 02:23:57 +0900

    add new enemies

--- packages/characters/enemies.ts
+++ packages/characters/enemies.ts
@@ -30,10 +30,68 @@ export const enemyUnitConfigs: EnemyUnitConfig[] = [
     attackRange: 64,
     structureTargetPriority: "gate"
   },
+  // 鬼火（青）
+  {
+    id: "enemy_onibi_blue",
+    charachip: "onibi_blue.png",
+    charachipConfig: {
+      frameWidth: 20,
+      frameHeight: 28,
+      displayWidth: 32
+    },
+    maxHealth: 15,
+    speed: 32,
+    attackDamage: 1,
+    attackRange: 64,
+    structureTargetPriority: "gate"
+  },
+  // 鬼火（黄）
+  {
+    id: "enemy_onibi_yellow",
+    charachip: "onibi_yellow.png",
+    charachipConfig: {
+      frameWidth: 20,
+      frameHeight: 28,
+      displayWidth: 32
+    },
+    maxHealth: 15,
+    speed: 32,
+    attackDamage: 1,
+    attackRange: 64,
+    structureTargetPriority: "gate"
+  },
     // 赤鬼
   {
     id: "enemy_red_oni",
     charachip: "red_oni.png",
+      charachipConfig: {
+      frameWidth: 20,
+      frameHeight: 28,
+      displayWidth: 52
+    },
+    maxHealth: 200,
+    speed: 32,
+    attackDamage: 5,
+    attackRange: 20,
+    "structureTargetPriority": "any"
+  },
+  {
+    id: "enemy_blue_oni",
+    charachip: "blue_oni.png",
+      charachipConfig: {
+      frameWidth: 20,
+      frameHeight: 28,
+      displayWidth: 52
+    },
+    maxHealth: 200,
+    speed: 32,
+    attackDamage: 5,
+    attackRange: 20,
+    "structureTargetPriority": "any"
+  },
+  {
+    id: "enemy_yellow_oni",
+    charachip: "yellow_oni.png",
       charachipConfig: {
       frameWidth: 20,
       frameHeight: 28,
